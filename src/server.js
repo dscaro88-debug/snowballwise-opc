461f56efd27556b1e9e411686786c7400f993966
dscaro88-debug <{"message":"Not Found","documentation_url":"https://docs.github.com/rest/users/emails#list-email-addresses-for-the-authenticated-user","status":"404"}>
2026-08-06 20:52:04 +0800

    fix: CORS allow DELETE for admin cross-origin

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
     ) {
       res.setHeader('Access-Control-Allow-Origin', origin);
       res.setHeader('Access-Control-Allow-Credentials', 'true');
-      res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+      res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,DELETE');
       res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
     }
   } catch {}
